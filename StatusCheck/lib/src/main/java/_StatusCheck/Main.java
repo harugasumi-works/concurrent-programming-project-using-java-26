9c2fb1dfacd8f109bfeb924fe198466f22925c02
harugasumi-works <tokino.yanagi@gmail.com>
2026-08-12 21:59:39 +0900

    Second day of project
    Accomplished steps:
    - Use java.net.http instead of Socket
    - Header can bypass most WAF, that is enough

--- StatusCheck/lib/src/main/java/_StatusCheck/Main.java
+++ StatusCheck/lib/src/main/java/_StatusCheck/Main.java
@@ -1,59 +1,56 @@
 package _StatusCheck;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.MalformedURLException;
-import java.net.Socket;
-import java.net.SocketAddress;
 import java.net.URI;
-import java.net.UnknownHostException;
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
 
 public class Main {
+	
+	private static final HttpClient client = HttpClient.newBuilder()
+			.version(Version.HTTP_3)
+            .connectTimeout(Duration.ofSeconds(10))
+            .build(); 
 
 	public static void main(String[] args) {
 
 		List<ScanRequest> request = List.of(
-				new ScanRequest("www.google.com"),
-				new ScanRequest("www.facebook.com"),
-				new ScanRequest("www.youtube.com"));
+				new ScanRequest("www.facebook.com/"));
 		
 					
-
-		// Clean Stream pipeline
 		List<ScanResult> results = request.stream()
 		    .map(req -> new ScanResult(req, executeScan(req)))
 		    .toList();
 		
-		System.out.println(results);
+		IO.println(results);
 	}
 	
-	public static Outcome testSocket(String host, int port) {		
-			SocketAddress socketAddress = new InetSocketAddress(host, port);
-			try (Socket sock = new Socket();) {
-				Instant start = Instant.now();
-				sock.connect(socketAddress, 3000);
-				Instant end = Instant.now();
-				return new Success(200, Duration.between(start, end).toMillis());
-				} catch (UnknownHostException noHost) {
-					return new Fail("Server not found: " + noHost.getMessage());
-				} catch (IOException noConn) {
-					return new Fail("I/O error connecting to server: " + noConn.getMessage());
-				}
-		}
-		
-		
+	
 	private static Outcome executeScan(ScanRequest req) {
-	    try {
-	        URI myURI = URI.create("https://" + req.URL());
-	        return testSocket(myURI.getHost(), myURI.toURL().getDefaultPort());
-	    } catch (MalformedURLException malf) {
-	        return new Fail("The URL is malformed: " + malf.getMessage());
-	    }
-	}
+        HttpRequest request = HttpRequest.newBuilder()
+        		 .uri(URI.create("https://" + req.URL()))
+        	        .timeout(Duration.ofSeconds(5))
+        	        .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36")
+        	        .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
+        	        .header("Accept-Language", "en-US,en;q=0.9")
+        	        .GET()
+        	        .build();
 
+		try {
+			Instant start = Instant.now();
+			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			Instant end = Instant.now();
+			return new Success(response.statusCode(), Duration.between(start, end).toMillis());
+		} catch (IOException | InterruptedException e) {
+			return new Fail("The connection was disrupted: " + e.getMessage());
+		}
+        
+	}
 	}
 	
 
